9d7fad4a18af57f80171d62eaacaf751063b2029
Savio Sena <savio.sena@gmail.com>
2024-04-11 08:25:34 -0300

    Add examples for chapter-8 (flow of control).

---
+++ examples/destructuring.rs
@@ -0,0 +1,205 @@
+// destructuring.rs, Examples from the book "Rust by Example"
+// Copyright (C) 2024, Savio Sena <savio.sena@gmail.com>
+
+fn match_tuples() {
+    let triple = (0, -2, 3);
+    // TODO ^ Try different values for `triple`
+
+    println!("Tell me about {:?}", triple);
+    // Match can be used to destructure a tuple
+    match triple {
+        // Destructure the second and third elements
+        (0, y, z) => println!("First is `0`, `y` is {:?}, and `z` is {:?}", y, z),
+        (1, ..) => println!("First is `1` and the rest doesn't matter"),
+        (.., 2) => println!("last is `2` and the rest doesn't matter"),
+        (3, .., 4) => println!("First is `3`, last is `4`, and the rest doesn't matter"),
+        // `..` can be used to ignore the rest of the tuple
+        _ => println!("It doesn't matter what they are"),
+        // `_` means don't bind the value to a variable
+    }
+}
+
+fn match_slices() {
+    // Try changing the values in the array, or make it a slice!
+    let array = [1, -2, 6];
+
+    match array {
+        // Binds the second and the third elements to the respective variables
+        [0, second, third] => println!("array[0] = 0, array[1] = {}, array[2] = {}", second, third),
+
+        // Single values can be ignored with _
+        [1, _, third] => println!(
+            "array[0] = 1, array[2] = {} and array[1] was ignored",
+            third
+        ),
+
+        // You can also bind some and ignore the rest
+        [-1, second, ..] => println!(
+            "array[0] = -1, array[1] = {} and all the other ones were ignored",
+            second
+        ),
+        // The code below would not compile
+        // [-1, second] => ...
+
+        // Or store them in another array/slice (the type depends on
+        // that of the value that is being matched against)
+        [3, second, tail @ ..] => println!(
+            "array[0] = 3, array[1] = {} and the other elements were {:?}",
+            second, tail
+        ),
+
+        // Combining these patterns, we can, for example, bind the first and
+        // last values, and store the rest of them in a single array
+        [first, middle @ .., last] => println!(
+            "array[0] = {}, middle = {:?}, array[2] = {}",
+            first, middle, last
+        ),
+    }
+}
+
+#[allow(dead_code)]
+enum Color {
+    // These 3 are specified solely by their name.
+    Red,
+    Blue,
+    Green,
+    // These likewise tie `u32` tuples to different names: color models.
+    RGB(u32, u32, u32),
+    HSV(u32, u32, u32),
+    HSL(u32, u32, u32),
+    CMY(u32, u32, u32),
+    CMYK(u32, u32, u32, u32),
+}
+
+fn match_enums() {
+    let color = Color::RGB(122, 17, 40);
+    // TODO ^ Try different variants for `color`
+
+    println!("What color is it?");
+    // An `enum` can be destructured using a `match`.
+    match color {
+        Color::Red => println!("The color is Red!"),
+        Color::Blue => println!("The color is Blue!"),
+        Color::Green => println!("The color is Green!"),
+        Color::RGB(r, g, b) => println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) => println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+        Color::HSL(h, s, l) => println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) => println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) => println!(
+            "Cyan: {}, magenta: {}, yellow: {}, key (black): {}!",
+            c, m, y, k
+        ),
+        // Don't need another arm because all variants have been examined
+    }
+}
+
+fn match_ptrs() {
+    // Assign a reference of type `i32`. The `&` signifies there
+    // is a reference being assigned.
+    let reference = &4;
+
+    match reference {
+        // If `reference` is pattern matched against `&val`, it results
+        // in a comparison like:
+        // `&i32`
+        // `&val`
+        // ^ We see that if the matching `&`s are dropped, then the `i32`
+        // should be assigned to `val`.
+        &val => println!("Got a value via destructuring: {:?}", val),
+    }
+
+    // To avoid the `&`, you dereference before matching.
+    match *reference {
+        val => println!("Got a value via dereferencing: {:?}", val),
+    }
+
+    // What if you don't start with a reference? `reference` was a `&`
+    // because the right side was already a reference. This is not
+    // a reference because the right side is not one.
+    let _not_a_reference = 3;
+
+    // Rust provides `ref` for exactly this purpose. It modifies the
+    // assignment so that a reference is created for the element; this
+    // reference is assigned.
+    let ref _is_a_reference = 3;
+
+    // Accordingly, by defining 2 values without references, references
+    // can be retrieved via `ref` and `ref mut`.
+    let value = 5;
+    let mut mut_value = 6;
+
+    // Use `ref` keyword to create a reference.
+    match value {
+        ref r => println!("Got a reference to a value: {:?}", r),
+    }
+
+    // Use `ref mut` similarly.
+    match mut_value {
+        ref mut m => {
+            // Got a reference. Gotta dereference it before we can
+            // add anything to it.
+            *m += 10;
+            println!("We added 10. `mut_value`: {:?}", m);
+        }
+    }
+}
+
+fn match_structs() {
+    struct Foo {
+        x: (u32, u32),
+        y: u32,
+    }
+
+    // Try changing the values in the struct to see what happens
+    let foo = Foo { x: (1, 2), y: 3 };
+
+    match foo {
+        Foo { x: (1, b), y } => println!("First of x is 1, b = {},  y = {} ", b, y),
+
+        // you can destructure structs and rename the variables,
+        // the order is not important
+        Foo { y: 2, x: i } => println!("y is 2, i = {:?}", i),
+
+        // and you can also ignore some variables:
+        Foo { y, .. } => println!("y = {}, we don't care about x", y),
+        // this will give an error: pattern does not mention field `x`
+        //Foo { y } => println!("y = {}", y),
+    }
+
+    let faa = Foo { x: (1, 2), y: 3 };
+
+    // You do not need a match block to destructure structs:
+    let Foo { x: x0, y: y0 } = faa;
+    println!("Outside: x0 = {x0:?}, y0 = {y0}");
+
+    // Destructuring works with nested structs as well:
+    struct Bar {
+        foo: Foo,
+    }
+
+    let bar = Bar { foo: faa };
+    let Bar {
+        foo: Foo {
+            x: nested_x,
+            y: nested_y,
+        },
+    } = bar;
+    println!("Nested: nested_x = {nested_x:?}, nested_y = {nested_y:?}");
+}
+
+fn main() {
+    println!("-=- match_tuples() -=-");
+    match_tuples();
+
+    println!("-=- match_slices() -=-");
+    match_slices();
+
+    println!("-=- match_enums() -=-");
+    match_enums();
+
+    println!("-=- match_ptrs() -=-");
+    match_ptrs();
+
+    println!("-=- match_structs() -=-");
+    match_structs();
+}
---
+++ examples/flow.rs
@@ -0,0 +1,206 @@
+// flow.rs, Examples from the book "Rust by Example"
+// Copyright (C) 2024, Savio Sena <savio.sena@gmail.com>
+
+#[allow(unused_variables)]
+
+fn if_else() {
+    let n = 5;
+
+    let m = if n <= 0 { 10 * n } else { n / 2 };
+
+    println!("{n} {m}");
+}
+
+#[allow(unreachable_code, unused_labels)]
+
+fn labels() {
+    'outer: loop {
+        println!("labels(): outer.");
+        'inner: loop {
+            println!("labels(): inner.");
+            break 'outer;
+        }
+    }
+    println!("labels(): end.");
+}
+
+fn loop_ret() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2; // return from loop
+        }
+    };
+
+    assert_eq!(result, 20);
+    println!("result = {result}");
+}
+
+fn for_match_str() {
+    let names = vec!["foo", "bar", "far"];
+    for name in names.iter() {
+        match name {
+            &"far" => println!("string found"),
+            _ => print!(""),
+        }
+    }
+}
+
+fn iter_mut() {
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "Savio",
+            &mut s => s,
+        }
+    }
+    println!("names: {:?}", names);
+}
+
+#[allow(dead_code)]
+enum Temperature {
+    Celsius(i32),
+    Fahrenheit(i32),
+}
+
+fn guards() {
+    let temperature = Temperature::Celsius(35);
+    match temperature {
+        Temperature::Celsius(t) if t > 30 => println!("{}C is above 30 Celsius", t),
+        // The `if condition` part ^ is a guard
+        Temperature::Celsius(t) => println!("{}C is equal to or below 30 Celsius", t),
+
+        Temperature::Fahrenheit(t) if t > 86 => println!("{}F is above 86 Fahrenheit", t),
+        Temperature::Fahrenheit(t) => println!("{}F is equal to or below 86 Fahrenheit", t),
+    }
+}
+
+fn binding() {
+    fn age() -> u32 {
+        15
+    }
+    match age() {
+        0 => println!("I haven't celebrated my first birthday yet"),
+        n @ 1..=12 => println!("I'm a child of age {:?}", n),
+        n @ 13..=19 => println!("I'm a teen of age {:?}", n),
+        n => println!("I'm an old person of age {:?}", n),
+    }
+
+    fn some_number() -> Option<u32> {
+        Some(42)
+    }
+    match some_number() {
+        Some(n @ 42) => println!("The Answer: {}!", n),
+        Some(n) => println!("Not interesting... {}", n),
+        _ => (),
+    }
+}
+
+fn if_let() {
+    let number = Some(7);
+    let letter: Option<i32> = None;
+
+    if let Some(i) = number {
+        println!("Matched {:?}!", i);
+    }
+
+    if let Some(i) = letter {
+        println!("Matched {:?}!", i);
+    } else {
+        // Destructure failed. Change to the failure case.
+        println!("Didn't match a number. Let's go with a letter!");
+    }
+
+    enum Foo {
+        Bar,
+        Baz(u32),
+    }
+
+    let a = Foo::Bar;
+    let b = Foo::Baz(100);
+
+    if let Foo::Bar = a {
+        println!("a is Foo::Bar");
+    }
+
+    if let Foo::Baz(value) = b {
+        println!("c is {}", value);
+    }
+
+    // Binding also works with `if let`
+    if let Foo::Baz(value @ 100) = b {
+        println!("c is one hundred: {value}");
+    }
+}
+
+use std::str::FromStr;
+
+fn get_count_item(s: &str) -> (u64, &str) {
+    let mut it = s.split(' ');
+    let (Some(count_str), Some(item)) = (it.next(), it.next()) else {
+        panic!("Can't segment count item pair: '{s}'");
+    };
+    let Ok(count) = u64::from_str(count_str) else {
+        panic!("Can't parse integer: '{count_str}'");
+    };
+    (count, item)
+}
+
+/// With `let-else`, a refutable pattern can match and bind variables in the
+/// surrounding scope like a normal `let`, or else diverge (e.g. `break`,
+/// `return`, `panic!`) when the pattern doesn't match.
+///
+/// 🛈 stable since: rust 1.65
+/// 🛈 you can target specific edition by compiling like this rustc --edition=2021 main.rs
+
+fn let_else() {
+    assert_eq!(get_count_item("3 chairs"), (3, "chairs"));
+}
+
+fn while_let() {
+    let mut optional = Some(0);
+    while let Some(i) = optional {
+        if i > 4 {
+            println!("Greater than 4, quit!");
+            optional = None;
+        } else {
+            println!("`i` is `{:?}`. Try again.", i);
+            optional = Some(i + 1);
+        }
+    }
+}
+
+fn main() {
+    println!("-=- if_else() -=-");
+    if_else();
+
+    println!("-=- labels() -=-");
+    labels();
+
+    println!("-=- loop_ret() -=-");
+    loop_ret();
+
+    println!("-=- for_match_str() -=-");
+    for_match_str();
+
+    println!("-=- iter_mut() -=-");
+    iter_mut();
+
+    println!("-=- guards() -=-");
+    guards();
+
+    println!("-=- binding() -=-");
+    binding();
+
+    println!("-=- if_let() -=-");
+    if_let();
+
+    println!("-=- let-else() -=-");
+    let_else();
+
+    println!("-=- while_let() -=-");
+    while_let();
+}
