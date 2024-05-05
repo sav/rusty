540604387fb16083afd4cf2c1e631b67d0b22ef2
Savio Sena <savio.sena@gmail.com>
2024-05-05 05:40:29 -0300

    Add examples for chapter-22 (unsafe/inline-asm).
    Signed-off-by: Savio Sena <savio.sena@gmail.com>

---
+++ examples/asm1.rs
@@ -0,0 +1,87 @@
+// asm1.rs, Examples from the book "Rust by Example"
+// Copyright (C) 2024, Savio Sena <savio.sena@gmail.com>
+
+//! # Inline assembly
+//!
+//! - [https://doc.rust-lang.org/rust-by-example/unsafe/asm.html]
+//! - [https://doc.rust-lang.org/stable/reference/inline-assembly.html]
+
+use std::arch::asm;
+
+fn mul(a: u64, b: u64) -> u128 {
+    let lo: u64;
+    let hi: u64;
+
+    unsafe {
+        asm!(
+            // The x86 mul instruction takes rax as an implicit input and writes
+            // the 128-bit result of the multiplication to rax:rdx.
+            "mul {}",
+            in(reg) a,
+            inlateout("rax") b => lo,
+            lateout("rdx") hi
+        );
+    }
+
+    ((hi as u128) << 64) + lo as u128
+}
+
+fn main() {
+    unsafe {
+        asm!("nop");
+    }
+
+    let x: u64;
+    unsafe {
+        asm!("mov {}, 5", out(reg) x);
+    }
+    assert_eq!(x, 5);
+
+    let i: u64 = 3;
+    let o: u64;
+    unsafe {
+        asm!(
+            "mov {0}, {1}",
+            "add {0}, 5",
+            out(reg) o,
+            in(reg) i,
+        );
+    }
+    assert_eq!(o, 8);
+
+    let mut x: u64 = 3;
+    unsafe {
+        asm!("add {0}, 5", inout(reg) x);
+    }
+    assert_eq!(x, 8);
+
+    let x: u64 = 3;
+    let y: u64;
+    unsafe {
+        asm!("add {0}, 5", inout(reg) x => y);
+    }
+    assert_eq!(y, 8);
+
+    let mut a: u64 = 4;
+    let b: u64 = 4;
+    let c: u64 = 4;
+    unsafe {
+        asm!(
+            "add {0}, {1}",
+            "add {0}, {2}",
+            inout(reg) a,
+            in(reg) b,
+            in(reg) c,
+        );
+    }
+    assert_eq!(a, 12);
+
+    let mut a: u64 = 4;
+    let b: u64 = 4;
+    unsafe {
+        asm!("add {0}, {1}", inlateout(reg) a, in(reg) b);
+    }
+    assert_eq!(a, 8);
+
+    assert_eq!(mul(10, 10), 100u128);
+}
