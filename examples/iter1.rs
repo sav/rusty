ea7fa154dc9b13a729703d32f85085165c4aa425
Savio Sena <savio.sena@gmail.com>
2024-05-18 21:05:46 -0300

    Add case to `iter1.rs`.
    Signed-off-by: Savio Sena <savio.sena@gmail.com>

--- examples/iter1.rs
+++ examples/iter1.rs
@@ -81,6 +81,18 @@ fn iter_aux() {
     }
 }
 
+fn iter_zip() {
+    let a1 = [1, 2, 3];
+    let a2 = [4, 5, 6];
+
+    let mut iter = a1.iter().zip(a2.iter());
+
+    assert_eq!(iter.next(), Some((&1, &4)));
+    assert_eq!(iter.next(), Some((&2, &5)));
+    assert_eq!(iter.next(), Some((&3, &6)));
+    assert_eq!(iter.next(), None);
+}
+
 fn iter_into() {
     let a = 5;
 
@@ -111,4 +123,7 @@ fn main() {
 
     println!("-=- iter_into() -=-");
     iter_into();
+
+    println!("-=- iter_zip() -=-");
+    iter_zip();
 }
